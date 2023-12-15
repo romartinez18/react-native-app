c505558667b6bc409c9e0ed5edcea9e697cbd69b
Ronald Martinez <rmartinez@integralplm.com>
2023-12-14 19:19:48 -0500

    Codebase repository

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BasicTypes } from './types/BasicTypes';
+
+const App = () => {
+  return (
+    <div className='mt-2'>
+        <h1>My App</h1>
+        <BasicTypes />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/types/BasicTypes.tsx
@@ -0,0 +1,14 @@
+export const BasicTypes = () => {
+    let name: string = "abc";
+    let edad = 35;
+    let listNames : string[] = [];
+
+    listNames.push('ABC')
+
+    return (
+        <>
+            <h3>Basic Types - {name}</h3>
+        </>
+    )
+}
+
